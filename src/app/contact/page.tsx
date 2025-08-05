ccefbcabc77c9188eba12a334356b584a1fd9dc3
kerrytooley <kj_tooley@hotmail.com>
2025-08-05 22:39:38 +0100

    contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import NavigationButton from "@/components/NavigationButton";
+import SectionTitle, { Header, SectionSubtitle, Text } from "@/components/SectionTitles";
+import { colors } from "@/styles/theme";
+
+export default function ContactPage() {
+  return (
+    <>
+      <div
+        style={{
+          backgroundColor: colors.light,
+          padding: "1rem",
+          width: "90vw",
+          height: "300px",
+          maxWidth: "1000px",
+          margin: "1rem auto",
+
+        }}
+      >
+        <SectionTitle style={{ marginTop: 0 }}>Contact Us</SectionTitle>
+        <div style={{ display: "flex", flexDirection: "row", padding: "1rem" }}>
+          <div style={{ flex: 1 }}>
+            <Text>
+              Contact us for quotes and information on bat ecology surveys. Our
+              team is ready to assist you with expert insights and tailored
+              solutions for your needs. Reach out today to learn more about how
+              we can help you.
+            </Text>
+          </div>
+          <div style={{ flex: 1}}>
+            <Header style={{textAlign: "center"}} >
+              <a href="mailto:admin@ectoecology.com?subject=Survey%20Request&body=Hi%20there,%0AI'd%20like%20to%20book%20a%20survey.">
+                {" "}
+                admin@ectoecology.com
+              </a>
+            </Header>
+          </div>
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <NavigationButton type="sales" />
+      </div>
+    </>
+  );
+}
